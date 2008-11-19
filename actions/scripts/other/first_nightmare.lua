ff6fe2fc0705bc8263eddcd82c06db70452c42e2
Guilherme Stresser <bowaroz@darghos.com.br>
2008-11-19 20:05:21 +0000

    -Arrumado problema do second_nightmare addon não sumir; -Modificado nome do item.
    git-svn-id: svn://darghos.com.br/darghos/data/trunk/8.3@313 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- actions/scripts/other/first_nightmare.lua
+++ actions/scripts/other/first_nightmare.lua
@@ -1,12 +1,13 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 
 	if getPlayerStorageValue(cid, 258258) == -1 then
-	
 		doCreatureSay(cid,'Congratulations! Now you have the First Nightmare addons! ' , TALKTYPE_ORANGE_1)
 		doPlayerAddOutfit(cid, 269,1)
 		doPlayerAddOutfit(cid, 268,1)
 		doSendMagicEffect(getPlayerPosition(cid),11)
 		doSendMagicEffect({x = getPlayerPosition(cid).x + 1, y = getPlayerPosition(cid).y + 1, z = getPlayerPosition(cid).z},11) 
+		doSendMagicEffect({x = getPlayerPosition(cid).x - 1, y = getPlayerPosition(cid).y + 1, z = getPlayerPosition(cid).z},11)
+		doSendMagicEffect({x = getPlayerPosition(cid).x + 1, y = getPlayerPosition(cid).y - 1, z = getPlayerPosition(cid).z},11)
 		doSendMagicEffect({x = getPlayerPosition(cid).x - 1, y = getPlayerPosition(cid).y - 1, z = getPlayerPosition(cid).z},11) 
 		doSendMagicEffect({x = getPlayerPosition(cid).x, y = getPlayerPosition(cid).y - 1, z = getPlayerPosition(cid).z},11) 
 		doSendMagicEffect({x = getPlayerPosition(cid).x - 1, y = getPlayerPosition(cid).y, z = getPlayerPosition(cid).z},11)
