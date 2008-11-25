217a02cab57570e1db4e67b1e9c22dac712bebbb
Renan Cunha <renan1bara@gmail.com>
2008-11-25 19:47:25 +0000

    *Tirado o comando print do code, o mesmo é inutil e é uma carga a mais no console do executavel
    git-svn-id: svn://darghos.com.br/darghos/data/trunk/8.3@362 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- actions/scripts/switchs/elevador_mina.lua
+++ actions/scripts/switchs/elevador_mina.lua
@@ -9,13 +9,11 @@ function onUse(cid, item, frompos, item2, topos)
 	elevadorPos = {x = 1912, y = 1230, z = 10, stackpos=1} -- posição aonde será criado/deletado o teleport do elevador
 
 	if item.uid == uniqueID and item.itemid == 1945 then
-		print("O jogador " .. getPlayerName(cid) .. " subiu no elevador.")
 		doRemoveItem(getThingfromPos(elevadorPos).uid)
 		doCreateTeleport(1387,firstFloor,elevadorPos)
 		doTransformItem(item.uid,item.itemid+1)
 		doPlayerSendTextMessage(cid,22,'Agora o elevador irá subir.')
 	elseif item.uid == uniqueID and item.itemid == 1946 then
-		print("O jogador " .. getPlayerName(cid) .. " deceu no elevador.")
 		doRemoveItem(getThingfromPos(elevadorPos).uid)
 		doCreateTeleport(1387,thirdFloor,elevadorPos)
 		doTransformItem(item.uid,item.itemid-1)
