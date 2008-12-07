0c23c7243025c2c376b57748431254aa6c349b7a
Renan Cunha <renan1bara@gmail.com>
2008-12-07 03:57:45 +0000

    *Pack POI MONSTERS update para novo estilo de Experiencie/ Entrada da POI concertada
    git-svn-id: svn://darghos.com.br/darghos/data/trunk/8.3@451 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- movements/scripts/requestVocationPOI.lua
+++ movements/scripts/requestVocationPOI.lua
@@ -1,4 +1,3 @@
------ Entrada POI QUEST
 function onStepIn(cid, item, pos) 
 
 
@@ -6,31 +5,50 @@ function onStepIn(cid, item, pos)
 	local idDruid = 5542
 	local idSorc = 5544
 	local idPaladin = 5543
-	local vocation = getPlayerVocation(cid)
+	local PVoc = getPlayerVocation(cid)
 	local NovaPos = {x= 386, y= 447, z= 9}
-		
-	if item.actionid == idKnight and vocation ~= 4 or vocation ~= 8 then
+
+	if item.actionid == idKnight then
+		if PVoc == 4 or PVoc == 8 then
+			
+		else
 		doTeleportThing(cid, NovaPos)
-		doPlayerSendTextMessage(cid, MSG_INFO_DESCR, "Only knights can walk in this templarium.")
-	else
-	return true
+		doSendMagicEffect(NovaPos, 11)
+        doPlayerSendTextMessage(cid,MESSAGE_EVENT_ADVANCE,'Only knights enter here.')
+		doSendAnimatedText(NovaPos, '\\o/', 180)
+		end
 	end
-	if item.actionid == idDruid and vocation ~= 2 or vocation ~= 6 then
-		doTeleportTing(cid, NovaPos)
-		doPlayerSendTextMessage(cid, MSG_INFO_DESCR, "Only druids can walk in this templarium.")
-	else
-	return true
-	end
-	if item.actionid == isSorc and vocation ~= 1 or vocation ~= 5 then
-		doTeleportTing(cid, NovaPos)
-		doPlayerSendTextMessage(cid, MSG_INFO_DESCR, "Only sorcerers can walk in this templarium.")	
-	else
-	return true
-	end
-	if item.actionid == idPaladin and vocation ~= 3 or vocation ~= 7 then
-		doTeleportTing(cid, NovaPos)
-		doPlayerSendTextMessage(cid, MSG_INFO_DESCR, "Only paladins can walk in this templarium.")
-	else
-	return true
+	
+	if item.actionid == idDruid then
+		if PVoc == 2 or PVoc == 6 then
+			
+		else
+		doTeleportThing(cid, NovaPos)
+		doSendMagicEffect(NovaPos, 11)
+        doPlayerSendTextMessage(cid,MESSAGE_EVENT_ADVANCE,'Only druids enter here.')
+		doSendAnimatedText(NovaPos, '\\o/', 180)
+		end
+	end	
+
+	if item.actionid == idSorc then
+		if PVoc == 1 or PVoc == 5 then
+			
+		else
+		doTeleportThing(cid, NovaPos)
+		doSendMagicEffect(NovaPos, 11)
+        doPlayerSendTextMessage(cid,MESSAGE_EVENT_ADVANCE,'Only sorcerers enter here.')
+		doSendAnimatedText(NovaPos, '\\o/', 180)
+		end
+	end	
+	
+	if item.actionid == idPaladin then
+		if PVoc == 3 or PVoc == 7 then
+			
+		else
+		doTeleportThing(cid, NovaPos)
+		doSendMagicEffect(NovaPos, 11)
+         doPlayerSendTextMessage(cid,MESSAGE_EVENT_ADVANCE,'Only paladins enter here.')
+		doSendAnimatedText(NovaPos, '\\o/', 180)
+		 end
 	end
 end
