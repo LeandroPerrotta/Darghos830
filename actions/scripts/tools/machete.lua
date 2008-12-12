1df95c25dbb61e04aeb5b899278f4fdb95479645
Renan Cunha <renan1bara@gmail.com>
2008-12-12 01:50:04 +0000

    *Correção na machete
    git-svn-id: svn://darghos.com.br/darghos/data/trunk/8.3@461 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- actions/scripts/tools/machete.lua
+++ actions/scripts/tools/machete.lua
@@ -2,7 +2,11 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if itemEx.itemid == 2782 then
 		doTransformItem(itemEx.uid, 2781)
 		doDecayItem(itemEx.uid)
-		return TRUE
+		return destroyItem(cid, itemEx, toPosition)
+	elseif itemEx.itemid == 1499 then
+		doTransformItem(itemEx.uid, 2671)
+		doSendAnimatedText(toPosition, '\\o/', 180)
+		doDecayItem(itemEx.uid)
+		return destroyItem(cid, itemEx, toPosition)
 	end
-	return destroyItem(cid, itemEx, toPosition)
 end
